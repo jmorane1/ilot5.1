937c6b631e5e7303bfc16d20bcb4b913c0832248
jmorane <jmorane@btslivh.eu>
2019-12-10 16:06:28 +0100

    Bonsoir

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 10 décembre 2019, 16:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<< " Bonjour tout le monde"<<endl;
+    
+    return 0;
+}
+
